6c741278762aaaae5c7913ae06919ad989c9d29d
meatsby <meatsby@gmail.com>
2021-12-16 20:08:09 +0900

    feat: 프로그램 흐름 추가 구현

--- src/main/java/subway/SubwayPathController.java
+++ src/main/java/subway/SubwayPathController.java
@@ -1,16 +1,21 @@
 package subway;
 
+import java.util.List;
+
+import subway.domain.SubwayMap;
 import subway.util.InputValidator;
 import subway.view.InputView;
 import subway.view.OutputView;
 
 public class SubwayPathController {
 	public void run() {
+		SubwayMap subwayMap = new SubwayMap();
+
 		while (true) {
 			OutputView.mainPage();
 			String mainCommend = getMainCommend();
 			if (mainCommend.equals("1")) {
-				operatePathSearch();
+				operatePathSearch(subwayMap);
 			}
 			if (mainCommend.equals("Q")) {
 				return;
@@ -18,15 +23,29 @@ public class SubwayPathController {
 		}
 	}
 
-	private void operatePathSearch() {
+	private void operatePathSearch(SubwayMap subwayMap) {
 		OutputView.menuPage();
 		String menuCommend = getMenuCommend();
 		if (menuCommend.equals("1")) {
-			searchShortestPath();
+			searchShortestPath(subwayMap);
 		}
 		if (menuCommend.equals("2")) {
-			searchFastestPath();
+			searchFastestPath(subwayMap);
 		}
+	}
+
+	private void searchShortestPath(SubwayMap subwayMap) {
+		List<String> stationNames = getStationNames();
+		List<String> shortestPath = subwayMap.searchShortestPath(stationNames);
+		List<Integer> pathInfo = subwayMap.getPathInfo(shortestPath);
+		OutputView.showPath(shortestPath, pathInfo);
+	}
+
+	private void searchFastestPath(SubwayMap subwayMap) {
+		List<String> stationNames = getStationNames();
+		List<String> fastestPath = subwayMap.searchFastestPath(stationNames);
+		List<Integer> pathInfo = subwayMap.getPathInfo(fastestPath);
+		OutputView.showPath(fastestPath, pathInfo);
 	}
 
 	private String getMainCommend() {
@@ -51,17 +70,14 @@ public class SubwayPathController {
 		}
 	}
 
-	private void searchShortestPath() {
-		String departure = InputView.departure();
-		String arrival = InputView.arrival();
-		subwayMap.getShortestPath(departure, arrival);
-		OutputView.showPath();
-	}
-
-	private void searchFastestPath() {
-		String departure = InputView.departure();
-		String arrival = InputView.arrival();
-		subwayMap.getFastestPath();
-		OutputView.showPath();
+	private List<String> getStationNames() {
+		List<String> stationNames = InputView.stationNames();
+		try {
+			InputValidator.isValidStationNames(stationNames);
+			return stationNames;
+		} catch (IllegalArgumentException e) {
+			OutputView.showError(e.getMessage());
+			return getStationNames();
+		}
 	}
 }
