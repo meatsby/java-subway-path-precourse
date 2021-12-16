6300da03f908d9bfc5fe2e1b58c4d503b65543c8
meatsby <meatsby@gmail.com>
2021-12-16 21:31:37 +0900

    feat: 메뉴화면 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -8,4 +8,12 @@ public class OutputView {
 		System.out.println("1. 경로 조회");
 		System.out.println("Q. 종료");
 	}
+
+	public static void menuPage() {
+		System.out.println();
+		System.out.println("## 경로 기준");
+		System.out.println("1. 최단 거리");
+		System.out.println("2. 최소 시간");
+		System.out.println("B. 돌아가기");
+	}
 }
