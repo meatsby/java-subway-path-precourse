c9c32482fb62a87faed6e0c2dd1cdd38ddc9efaa
meatsby <meatsby@gmail.com>
2021-12-16 21:31:11 +0900

    feat: 메인화면 출력 기능 구현

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,11 @@
+package subway.view;
+
+import java.util.List;
+
+public class OutputView {
+	public static void mainPage() {
+		System.out.println("## 메인 화면");
+		System.out.println("1. 경로 조회");
+		System.out.println("Q. 종료");
+	}
+}
